52346dd0a6cc74691ca35233c2c5d2704294b786
marins <tiagomarinsq97@gmail.com>
2019-04-09 06:59:09 -0300

    ...

--- app/src/main/java/com/example/marin/qrticket/activity/CadastrarUsuario.java
+++ app/src/main/java/com/example/marin/qrticket/activity/CadastrarUsuario.java
@@ -5,8 +5,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.marin.qrticket.R;
+import com.example.marin.qrticket.model.Usuario;
+import com.example.marin.qrticket.util.RetrofitUtil;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class CadastrarUsuario extends AppCompatActivity implements View.OnClickListener {
 
@@ -31,6 +38,27 @@ public class CadastrarUsuario extends AppCompatActivity implements View.OnClickL
 
     @Override
     public void onClick(View view) {
+        Usuario u = new Usuario();
+        u.setNome(nome.getText().toString());
+        u.setLogin(login.getText().toString());
+        u.setSenha(senha.getText().toString());
+        u.setPerfil(perfil);
+        u.setFlagAprovado(1);
 
+        RetrofitUtil retrofitUtil = RetrofitUtil.retrofit.create(RetrofitUtil.class);
+        final Call<Void> call = retrofitUtil.inserirUsuario(u);
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if (response.isSuccessful()){
+                    
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Toast.makeText(getBaseContext(), "Erro 404", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/marin/qrticket/util/RetrofitUtil.java
+++ app/src/main/java/com/example/marin/qrticket/util/RetrofitUtil.java
@@ -5,7 +5,9 @@ import com.example.marin.qrticket.model.Usuario;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 /**
@@ -14,9 +16,12 @@ import retrofit2.http.Path;
 
 public interface RetrofitUtil {
 
+    @POST("")
+    Call<Void> inserirUsuario(@Body Usuario usuario);
 
     @GET("")
     Call<Usuario> logar(@Path("login") String login, @Path("senha") String senha);
+
 
     public static final Retrofit retrofit = new Retrofit.Builder().baseUrl("http://10.0.2.2:8080/")
             .addConverterFactory(GsonConverterFactory.create()).build();
